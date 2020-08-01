d9055adc7401f55d2881818284bd1b7216bde484
operezcham <operezcham@gmail.com>
2020-07-31 22:11:59 -0500

    clock

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var clock = new Vue({
+    el: '#clock',
+    data: {
+        hide: false,
+        hours: '00',
+        minutes: '00',
+        seconds: '00'
+    }
+});
+function tick() {
+    var date = Date.now();
+    clock.data.hours = '0' + date.getHours();
+    clock.data.minutes = '0' + date.getMinutes();
+    clock.data.seconds = '0' + date.getSeconds();
+}
+setTimeout(tick, 1000);
